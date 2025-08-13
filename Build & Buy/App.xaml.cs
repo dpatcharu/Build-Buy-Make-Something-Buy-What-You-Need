4bd66f90a0e412befc8600c3f9a826fd3aabe140
OnePiece <patcharu@gmail.com>
2025-08-12 21:11:43 -0400

    intial setup

---
+++ Build & Buy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Build___Buy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Build & Buy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Build___Buy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
